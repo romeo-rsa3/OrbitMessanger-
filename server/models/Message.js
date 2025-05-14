c806f918c867e49d7b99bd8ca5ca8a8435b537b6
romeo-rsa3 <168164576+romeo-rsa3@users.noreply.github.com>
2025-05-14 03:30:48 +0200

    Update Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -4,6 +4,7 @@ const messageSchema = new mongoose.Schema({
   from: { type: String, required: true },
   to: { type: String },
   group: { type: String },
+  read: { type: Boolean, default: false },
   message: { type: String, required: true },
   type: { type: String, enum: ["private", "group"], required: true },
   timestamp: { type: Date, default: Date.now }
